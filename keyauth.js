e890bd83e3120b0c2ba276f96b404e3bb07d172e
olvsant <86132855+Gab1777@users.noreply.github.com>
2024-05-16 06:21:44 -0300

    DD Osama Fan Club

---
+++ keyauth.js
@@ -0,0 +1,7 @@
+public static api KeyAuthApp = new api(
+    name: "OsamaFan", // Application Name
+    ownerid: "ELG4UYmhMt", // Owner ID
+    secret: "5e1c105db6218d8b6057e6721e1227dce05650917bbeca6449720ed89b3da1c7", // Application Secret
+    version: "1.0" // Application Version, /*
+    path: @"Your_Path_Here" */ // see tutorial here https://www.youtube.com/watch?v=I9rxt821gMk&t=1s
+);
